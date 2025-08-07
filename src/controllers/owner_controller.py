4f5bd0316af71de0e1bc5e6f76937bcefb2bd3cc
TDV <zombietdv2701@gmail.com>
2025-08-07 21:16:06 +0700

    Thêm lớp UserControler. phương thức rent_parking_space

---
+++ src/controllers/owner_controller.py
@@ -0,0 +1,12 @@
+class UserController:
+    def __init__(self):
+        pass
+        
+    def rent_parking_space(self, user_id, parking_space_id):
+        """
+        Handle the logic for renting a parking space.
+        """
+        pass
+    
+    
+    
