1aae161272407791ad2a73b99d83ab9a2d39ea5e
VuongUTH <zombietdv2701@gmail.com>
2025-08-08 15:26:58 +0700

    sửa lỗi type

--- main.py
+++ main.py
@@ -1,18 +1,13 @@
 from src.database import parking_slot
-from config.setting import settings
 from src.controllers.admin_controller import admin
 
 
 if __name__ == "__main__":
-    # parking_slot.init_parking_slots(size = 10)
-    # database_management.clear_table("parking_slot")
-
     print(f"Number of rows: {parking_slot.count()}") 
     
     
     admin.track_availability()
     
     print("Exit!")
-    # admin.track_availability()
     
     
