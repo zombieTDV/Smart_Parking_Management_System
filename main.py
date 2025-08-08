62462b7e45fe2302302e4f681447e9ef7d526ae2
kien <lenguyenkien.th@gmail.com>
2025-08-08 15:23:59 +0700

    tao nguoi dung User

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from src.database import parking_slot
 from config.setting import settings
 from src.controllers.admin_controller import admin
+from src.controllers.owner_controller import User
 
 
 if __name__ == "__main__":
+    John = User()
     # parking_slot.init_parking_slots(size = 10)
-    # database_management.clear_table("parking_slot")
+    # # database_management.clear_table("parking_slot")
 
-    print(f"Number of rows: {parking_slot.count()}") 
+    # print(f"Number of rows: {parking_slot.count()}") 
     
     
-    admin.track_availability()
+    # admin.track_availability()
+
+    John.track_availability()
     
     print("Exit!")
     # admin.track_availability()
