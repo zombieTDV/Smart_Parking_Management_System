479c8b6e78725377f4d56f05fe1fea1d9330b9df
kien <lenguyenkien.th@gmail.com>
2025-08-09 08:53:45 +0700

    kien

--- main.py
+++ main.py
@@ -16,5 +16,25 @@ if __name__ == "__main__":
     John.track_availability()
     
     print("Exit!")
-    
+
+class User:
+    def __init__(self):
+        pass
+
+    def track_availability(self) -> None:
+        select = parking_slot.select_all()
+        print("=== Parking Status ===")
+        for i in range(parking_slot.count()):
+            if select[i][1] == 1:
+                print(f"ID: {select[i][0]}  ----- availability: Free")
+            else:
+                print(f"ID: {select[i][0]}  ----- availability: Occupied")
+
+    def check_available_slots(self) -> None:
+        select = parking_slot.select_all()
+        print("=== Available Parking Slots ===")
+        for i in range(parking_slot.count()):
+            if select[i][1] == 1:
+                print(f"ID: {select[i][0]} is Free")
+
     
