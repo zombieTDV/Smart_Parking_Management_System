7fa58f929465ea6712a8d1097e5cf1e35f04f3cf
VuongUTH <zombietdv2701@gmail.com>
2025-08-10 13:26:26 +0700

    dọn dẹp owner.py

--- src/Repositories/owner.py
+++ src/Repositories/owner.py
@@ -18,25 +18,3 @@ class CarOwner:
         print("\nChỗ đỗ còn trống:")
         for slot in free_slots:
             print(f"- ID: {slot[0]}") # type: ignore
-
-    # def pay_parking_fee(self):
-    #     """
-    #     Cho phép chủ xe kiểm tra phí và thanh toán giao dịch đang mở.
-    #     """
-    #     tr = transaction_repo.pay()
-    #     if not tr:
-    #         print("❌ Không có giao dịch đang chờ thanh toán.")
-    #         return
-
-    #     hours = tr.calculate_duration_hours()
-    #     fee = hours * settings.hourly_rates
-    #     print(f"\nThời gian đỗ: {hours} giờ")
-    #     print(f"Phí phải thanh toán: {fee} VND")
-
-    #     confirm = input("Thanh toán ngay? (y/n): ").strip().lower()
-    #     if confirm == 'y':
-    #         transaction_repo.close_transaction(tr.id, fee)
-    #         parking_slot.release_slot(tr.slot_id)
-    #         print("✅ Thanh toán thành công. Cảm ơn bạn đã sử dụng dịch vụ.")
-    #     else:
-    #         print("❌ Thanh toán đã hủy.")
